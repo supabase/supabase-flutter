c9798fc349b9d0c42bbeca676e958b1af7ddcf77
Vishesh Handa <me@vhanda.in>
2021-09-07 09:42:34 +0200

    Export the LocalStorage class
    Many projects use the analyzer's option of avoiding 'implementation_imports'.
    This rule needs to be explicitly ignored for importing LocalStorage.

--- lib/supabase_flutter.dart
+++ lib/supabase_flutter.dart
@@ -3,6 +3,7 @@
 /// See <https://github.com/supabase/supabase-flutter> to learn more
 library supabase_flutter;
 
+export 'src/local_storage.dart';
 export 'src/supabase.dart';
 export 'src/supabase_auth.dart';
 export 'src/supabase_auth_required_state.dart';
