40e507782de64a3f85acf13acf919924ccbe0122
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-08-11 23:52:28 +0900

    feat: developers can set custom headers and adding client info headers

---
+++ example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,19 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore_for_file: directives_ordering
+// ignore_for_file: lines_longer_than_80_chars
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:app_links_web/app_links_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(Registrar registrar) {
+  AppLinksPluginWeb.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
