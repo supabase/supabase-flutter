02d3dff1e4ee1fb9c64ff933992ac84326fde2d2
Gabriele Feudo <40292753+gabfeudo@users.noreply.github.com>
2023-01-11 09:29:18 +0100

    Feat/in app browser oauth (#323)
    * feat: added LaunchMode parameter for OAuth signIn
    
    * refactor: renamed `mode` param to `authScreenLaunchMode`
    
    * feat: set `inAppWebView` as default value for `authScreenLaunchMode` param
    
    * refactor: `authScreenLaunchMode` set back to `externalApplication` as default value
    
    * feat: exported `LaunchMode` from `url_launcher`
    
    Co-authored-by: Gabriele Feudo <gabryfeudo@gmai.com>

--- lib/src/supabase_auth.dart
+++ lib/src/supabase_auth.dart
@@ -283,6 +283,7 @@ extension GoTrueClientSignInProvider on GoTrueClient {
     Provider provider, {
     String? redirectTo,
     String? scopes,
+    LaunchMode authScreenLaunchMode = LaunchMode.externalApplication,
     Map<String, String>? queryParams,
   }) async {
     final res = await getOAuthSignInUrl(
@@ -294,7 +295,7 @@ extension GoTrueClientSignInProvider on GoTrueClient {
     final url = Uri.parse(res.url!);
     final result = await launchUrl(
       url,
-      mode: LaunchMode.externalApplication,
+      mode: authScreenLaunchMode,
       webOnlyWindowName: '_self',
     );
     return result;
