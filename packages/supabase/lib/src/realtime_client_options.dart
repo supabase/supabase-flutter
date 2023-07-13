7bf61508e79fc4c0b2a68e6e872466176e9fc579
KyleParker-Gongyuan <67570672+KyleParker-Gongyuan@users.noreply.github.com>
2023-07-13 06:03:01 -0700

    Events per second comment
    Not well documented.

--- packages/supabase/lib/src/realtime_client_options.dart
+++ packages/supabase/lib/src/realtime_client_options.dart
@@ -1,5 +1,6 @@
 /// {@template realtime_client_options}
-/// Options to pass to the RealtimeClient
+/// Options to pass to the RealtimeClient.
+/// Default is 10 per second
 /// {@endtemplate}
 class RealtimeClientOptions {
   /// How many events the RealtimeClient can push in a second
