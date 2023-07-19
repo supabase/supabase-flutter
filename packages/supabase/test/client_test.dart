7a51c7968615609e5935e0cbbc196dccb16e8063
Vinzent <vinzent03@proton.me>
2023-07-19 21:27:30 +0200

    test: add auth headers test

--- packages/supabase/test/client_test.dart
+++ packages/supabase/test/client_test.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:supabase/supabase.dart';
 import 'package:test/test.dart';
 
@@ -25,6 +28,46 @@ void main() {
       final xClientHeaderBeforeSlash =
           client.storage.headers['X-Client-Info']!.split('/').first;
       expect(xClientHeaderBeforeSlash, 'supabase-dart');
+    });
+  });
+
+  group('auth', () {
+    test('properly set Authorization header', () async {
+      final expiresAt =
+          DateTime.now().add(Duration(hours: 1)).millisecondsSinceEpoch ~/ 1000;
+      final accessToken = base64.encode(utf8.encode(json.encode(
+          {"exp": expiresAt, "sub": "1234567890", "role": "authenticated"})));
+
+      final sessionString =
+          '{"currentSession":{"access_token":"$accessToken","expires_in":3600,"refresh_token":"-yeS4omysFs9tpUYBws9Rg","token_type":"bearer","provider_token":null,"provider_refresh_token":null,"user":{"id":"4d2583da-8de4-49d3-9cd1-37a9a74f55bd","app_metadata":{"provider":"email","providers":["email"]},"user_metadata":{"Hello":"World"},"aud":"","email":"fake1680338105@email.com","phone":"","created_at":"2023-04-01T08:35:05.208586Z","confirmed_at":null,"email_confirmed_at":"2023-04-01T08:35:05.220096086Z","phone_confirmed_at":null,"last_sign_in_at":"2023-04-01T08:35:05.222755878Z","role":"","updated_at":"2023-04-01T08:35:05.226938Z"}},"expiresAt":$expiresAt}';
+
+      final mockServer = await HttpServer.bind('localhost', 0);
+      final client = SupabaseClient(
+        'http://${mockServer.address.host}:${mockServer.port}',
+        "supabaseKey",
+        autoRefreshToken: false,
+      );
+      await client.auth.recoverSession(sessionString);
+
+      // Make some requests
+      client.from("test").select().then((value) => null);
+      client.rpc("test").select().then((value) => null);
+      client.functions.invoke("test").then((value) => null);
+      client.storage.from("test").list().then((value) => null);
+
+      var count = 0;
+
+      // Check for every request if the Authorization header is set properly
+      await for (final req in mockServer) {
+        expect(
+            req.headers.value('Authorization')?.split(" ").last, accessToken);
+        count++;
+        if (count == 4) {
+          break;
+        }
+      }
+
+      mockServer.close();
     });
   });
 
