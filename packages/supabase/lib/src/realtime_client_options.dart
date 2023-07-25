9a30876b7a8efc55b6a857f537e7d7d86438c74b
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2023-07-25 11:25:39 +0900

    update comment

--- packages/supabase/lib/src/realtime_client_options.dart
+++ packages/supabase/lib/src/realtime_client_options.dart
@@ -1,9 +1,10 @@
 /// {@template realtime_client_options}
 /// Options to pass to the RealtimeClient.
-/// Default is 10 per second
 /// {@endtemplate}
 class RealtimeClientOptions {
   /// How many events the RealtimeClient can push in a second
+  ///
+  /// Defaults to 10 events per second
   final int? eventsPerSecond;
 
   /// {@macro realtime_client_options}
