72ffb9ee1a1386fb7ab8085b68cd9bc6f6d72c78
Vinzent <vinzent03@proton.me>
2023-11-30 02:26:20 +0100

    fix: sign out on already used refresh token (#740)

--- packages/gotrue/lib/src/gotrue_client.dart
+++ packages/gotrue/lib/src/gotrue_client.dart
@@ -981,7 +981,7 @@ class GoTrueClient {
       rethrow;
     } catch (error, stack) {
       if (error is AuthException) {
-        if (error.message == 'Invalid Refresh Token: Refresh Token Not Found') {
+        if (error.message.startsWith('Invalid Refresh Token:')) {
           await signOut();
         }
       }
