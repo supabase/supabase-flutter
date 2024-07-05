1fc399ea767caf3b1318b3bea974320f6ad64f46
Bob Ruiken <bob@synergi.so>
2024-07-05 16:38:31 +0300

    feat(SupabaseAuth): allow auth state change callbacks in init options

--- packages/supabase_flutter/lib/src/flutter_go_true_client_options.dart
+++ packages/supabase_flutter/lib/src/flutter_go_true_client_options.dart
@@ -3,6 +3,13 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class FlutterAuthClientOptions extends AuthClientOptions {
   final LocalStorage? localStorage;
 
+  /// Callback for receiving state change updates from the auth subscription.
+  final void Function(AuthState data)? onAuthStateChange;
+
+  /// Callback for receiving errors from the auth state change subscription.
+  /// Both the error and the stacktrace will be given as arguments.
+  final Function? onAuthError;
+
   /// If true, the client will start the deep link observer and obtain sessions
   /// when a valid URI is detected.
   final bool detectSessionInUri;
@@ -12,6 +19,8 @@ class FlutterAuthClientOptions extends AuthClientOptions {
     super.autoRefreshToken,
     super.pkceAsyncStorage,
     this.localStorage,
+    this.onAuthStateChange,
+    this.onAuthError,
     this.detectSessionInUri = true,
   });
 
@@ -21,6 +30,8 @@ class FlutterAuthClientOptions extends AuthClientOptions {
     LocalStorage? localStorage,
     GotrueAsyncStorage? pkceAsyncStorage,
     bool? detectSessionInUri,
+    void Function(AuthState data)? onAuthStateChange,
+    Function? onAuthError,
   }) {
     return FlutterAuthClientOptions(
       authFlowType: authFlowType ?? this.authFlowType,
@@ -28,6 +39,8 @@ class FlutterAuthClientOptions extends AuthClientOptions {
       localStorage: localStorage ?? this.localStorage,
       pkceAsyncStorage: pkceAsyncStorage ?? this.pkceAsyncStorage,
       detectSessionInUri: detectSessionInUri ?? this.detectSessionInUri,
+      onAuthStateChange: onAuthStateChange ?? this.onAuthStateChange,
+      onAuthError: onAuthError ?? this.onAuthError,
     );
   }
 }
