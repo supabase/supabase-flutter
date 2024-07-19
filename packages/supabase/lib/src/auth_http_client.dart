1b005ae782fc269c8cfb322dc722c5951644009f
dshukertjr <dshukertjr@gmail.com>
2024-07-20 00:46:50 +0900

    fix: add error message for when the session is missing

--- packages/supabase/lib/src/auth_http_client.dart
+++ packages/supabase/lib/src/auth_http_client.dart
@@ -14,10 +14,20 @@ class AuthHttpClient extends BaseClient {
       try {
         await _auth.refreshSession();
       } catch (error) {
+        final session = _auth.currentSession;
+        if (session == null) {
+          // No session to refresh.
+          throw AuthException('No session to make the API request.');
+        }
+        final expiresAt = session.expiresAt;
+        if (expiresAt == null) {
+          // No expiry time to refresh.
+          throw AuthException('Session does not contain exp claim.');
+        }
+
         // Failed to refresh the token.
-        final isExpiredWithoutMargin = DateTime.now().isAfter(
-            DateTime.fromMillisecondsSinceEpoch(
-                _auth.currentSession!.expiresAt! * 1000));
+        final isExpiredWithoutMargin = DateTime.now()
+            .isAfter(DateTime.fromMillisecondsSinceEpoch(expiresAt * 1000));
         if (isExpiredWithoutMargin) {
           // Throw the error instead of making an API request with an expired token.
           rethrow;
