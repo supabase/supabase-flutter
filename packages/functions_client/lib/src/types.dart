528a04d5d7d30b5e8cfde94d5a83da0d040df047
dshukertjr <dshukertjr@gmail.com>
2024-07-24 11:13:49 +0900

    delete unnecessary ignore line

--- packages/functions_client/lib/src/types.dart
+++ packages/functions_client/lib/src/types.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 import 'dart:typed_data';
 
@@ -32,7 +31,8 @@ class FunctionException {
   final dynamic details;
   final String? reasonPhrase;
 
-  const FunctionException({required this.status, this.details, this.reasonPhrase});
+  const FunctionException(
+      {required this.status, this.details, this.reasonPhrase});
 
   @override
   String toString() =>
