30191b266ba29418aa11c0d525e95f190cc8cfb9
Tyler <dshukertjr@gmail.com>
2024-07-24 11:11:25 +0900

    Update packages/functions_client/lib/src/types.dart
    Co-authored-by: Bruno D'Luka <45696119+bdlukaa@users.noreply.github.com>

--- packages/functions_client/lib/src/types.dart
+++ packages/functions_client/lib/src/types.dart
@@ -32,7 +32,7 @@ class FunctionException {
   final dynamic details;
   final String? reasonPhrase;
 
-  FunctionException({required this.status, this.details, this.reasonPhrase});
+  const FunctionException({required this.status, this.details, this.reasonPhrase});
 
   @override
   String toString() =>
