c47d56deff75fd918909648f2fde5f94f687995f
YumNumm <onory0917@gmail.com>
2024-09-28 23:22:50 +0900

    fix: change `dart.library.js` -> `dart.library.js_interop`

--- packages/yet_another_json_isolate/lib/yet_another_json_isolate.dart
+++ packages/yet_another_json_isolate/lib/yet_another_json_isolate.dart
@@ -1,3 +1,3 @@
 library yet_another_json_isolate;
 
-export 'src/_isolates_io.dart' if (dart.library.js) 'src/_isolates_web.dart';
+export 'src/_isolates_io.dart' if (dart.library.js_interop) 'src/_isolates_web.dart';
