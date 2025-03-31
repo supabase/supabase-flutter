a1be4e4bfb034b2910afb6c3c3812435e66f8ba2
Vinzent <vinzent03@proton.me>
2025-03-31 15:59:28 +0200

    refactor: rename external js field

--- packages/supabase_flutter/lib/src/hot_restart_cleanup_web.dart
+++ packages/supabase_flutter/lib/src/hot_restart_cleanup_web.dart
@@ -3,7 +3,7 @@ import 'dart:js_interop';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 @JS()
-external JSFunction? supabaseFlutterWSToClose;
+external JSFunction? supabaseFlutterClientToDispose;
 
 /// Store a function to properly dispose the previous [SupabaseClient] in
 /// the js context.
@@ -15,13 +15,13 @@ external JSFunction? supabaseFlutterWSToClose;
 /// errors and the fact that the events of the old connection may still be
 /// logged.
 void markClientToDispose(SupabaseClient client) {
-  void disconnect() {
+  void dispose() {
     client.realtime.disconnect(
         code: 1000, reason: 'Closed due to Flutter Web hot-restart');
     client.dispose();
   }
 
-  supabaseFlutterWSToClose = disconnect.toJS;
+  supabaseFlutterClientToDispose = dispose.toJS;
 }
 
 /// Disconnect the previous [SupabaseClient] if it exists.
@@ -29,8 +29,8 @@ void markClientToDispose(SupabaseClient client) {
 /// This is done by calling the function stored by
 /// [markClientToDispose] from the js context
 void disposePreviousClient() {
-  if (supabaseFlutterWSToClose != null) {
-    supabaseFlutterWSToClose!.callAsFunction();
-    supabaseFlutterWSToClose = null;
+  if (supabaseFlutterClientToDispose != null) {
+    supabaseFlutterClientToDispose!.callAsFunction();
+    supabaseFlutterClientToDispose = null;
   }
 }
