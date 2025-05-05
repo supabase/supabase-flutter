f33c9fe2f67fc83d19904a53e0b71e7cec5cee53
Guilherme Souza <guilherme@supabase.io>
2025-05-05 16:54:34 -0300

    feat: Add standard client headers (#1130)
    * feat: add standard client headers
    
    * verify if isn't web

--- packages/supabase/lib/src/constants.dart
+++ packages/supabase/lib/src/constants.dart
@@ -1,7 +1,21 @@
 import 'package:supabase/src/version.dart';
+import 'dart:io' show Platform;
+
+const bool kIsWeb = bool.fromEnvironment('dart.library.js_util');
 
 class Constants {
-  static const Map<String, String> defaultHeaders = {
+  static String? get platform {
+    return kIsWeb ? null : Platform.operatingSystem;
+  }
+
+  static String? get platformVersion {
+    return kIsWeb ? null : Platform.operatingSystemVersion;
+  }
+
+  static final Map<String, String> defaultHeaders = {
     'X-Client-Info': 'supabase-dart/$version',
+    if (platform != null) 'X-Supabase-Client-Platform': platform!,
+    if (platformVersion != null)
+      'X-Supabase-Client-Platform-Version': platformVersion!,
   };
 }
