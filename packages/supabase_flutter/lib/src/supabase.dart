2d12a6c5ad9305060bd18005e419cfbff8d99f9c
Vinzent <vinzent03@proton.me>
2025-06-12 22:57:14 +0200

    refactor: remove usage of cupertino client

--- packages/supabase_flutter/lib/src/supabase.dart
+++ packages/supabase_flutter/lib/src/supabase.dart
@@ -14,8 +14,6 @@ import 'package:supabase_flutter/src/supabase_auth.dart';
 import 'hot_restart_cleanup_stub.dart'
     if (dart.library.js_interop) 'hot_restart_cleanup_web.dart';
 
-import 'platform_http_io.dart'
-    if (dart.library.js_interop) 'platform_http_web.dart';
 import 'version.dart';
 
 final _log = Logger('supabase.supabase_flutter');
@@ -119,14 +117,6 @@ class Supabase with WidgetsBindingObserver {
         ),
       );
     }
-    if (realtimeClientOptions.webSocketTransport == null) {
-      final platformWebSocketChannel = getPlatformWebSocketChannel();
-      if (platformWebSocketChannel != null) {
-        realtimeClientOptions = realtimeClientOptions.copyWith(
-            webSocketTransport: (url, headers) =>
-                platformWebSocketChannel(url));
-      }
-    }
     _instance._init(
       url,
       anonKey,
@@ -205,16 +195,10 @@ class Supabase with WidgetsBindingObserver {
       ...Constants.defaultHeaders,
       if (customHeaders != null) ...customHeaders
     };
-    final Client platformHttpClient;
-    if (httpClient != null) {
-      platformHttpClient = httpClient;
-    } else {
-      platformHttpClient = getPlatformHttpClient();
-    }
     client = SupabaseClient(
       supabaseUrl,
       supabaseAnonKey,
-      httpClient: platformHttpClient,
+      httpClient: httpClient,
       headers: headers,
       realtimeClientOptions: realtimeClientOptions,
       postgrestOptions: postgrestOptions,
