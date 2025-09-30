34651c01ecad5e65f2f064cd19143a3428f0ee9a
Vinzent <vinzent03@proton.me>
2025-09-30 15:27:38 +0200

    chore: require app_links ^6.2.0 (#1241)

--- packages/supabase_flutter/test/deep_link_test.dart
+++ packages/supabase_flutter/test/deep_link_test.dart
@@ -1,6 +1,5 @@
 @TestOn('!browser')
 
-import 'package:app_links/app_links.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -12,34 +11,13 @@ void main() {
 
   group('Deep Link with PKCE code', () {
     late final PkceHttpClient pkceHttpClient;
-    late final bool mockEventChannel;
-
-    /// Check if the current version of AppLinks uses an explicit call to get
-    /// the initial link. This is only the case before version 6.0.0, where we
-    /// can find the getInitialAppLink function.
-    ///
-    /// CI pipeline is set so that it tests both app_links newer and older than v6.0.0
-    bool appLinksExposesInitialLinkInStream() {
-      try {
-        // before app_links 6.0.0
-        (AppLinks() as dynamic).getInitialAppLink;
-        return false;
-      } on NoSuchMethodError catch (_) {
-        return true;
-      }
-    }
 
     setUp(() async {
       pkceHttpClient = PkceHttpClient();
 
-      // Add initial deep link with a `code` parameter, use method channel if
-      // we are in a version of AppLinks that use the explcit method for
-      // getting the initial link. Otherwise we want to mock the event channel
-      // and put the initial link there.
-      mockEventChannel = appLinksExposesInitialLinkInStream();
       mockAppLink(
-        mockMethodChannel: !mockEventChannel,
-        mockEventChannel: mockEventChannel,
+        mockMethodChannel: false,
+        mockEventChannel: true,
         initialLink: 'com.supabase://callback/?code=my-code-verifier',
       );
       await Supabase.initialize(
@@ -62,9 +40,7 @@ void main() {
         () async {
       // Wait for the initial app link to be handled, as this is an async
       // process when mocking the event channel.
-      if (mockEventChannel) {
-        await Future.delayed(const Duration(milliseconds: 500));
-      }
+      await Future.delayed(const Duration(milliseconds: 500));
       expect(pkceHttpClient.requestCount, 1);
       expect(pkceHttpClient.lastRequestBody['auth_code'], 'my-code-verifier');
     });
