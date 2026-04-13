e4dfdd74b68f2ed7826d5e25421274ae0d1424b9
Bruno Siqueira <brunovsiq@gmail.com>
2026-04-13 11:05:53 +0100

    style(gotrue): apply dart format with 80-char line limit

--- packages/gotrue/lib/src/gotrue_client.dart
+++ packages/gotrue/lib/src/gotrue_client.dart
@@ -1402,7 +1402,8 @@ class GoTrueClient {
     // share its future.
     final active = _activeRefresh;
     if (active != null && active.refreshToken == refreshToken) {
-      _log.finer("Don't call refresh token, already in progress for same token");
+      _log.finer(
+          "Don't call refresh token, already in progress for same token");
       return active.completer.future;
     }
 
