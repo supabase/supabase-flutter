590586967096dc93e678fb8975929bb32931560a
Vinzent <hello@vinzentw.com>
2026-05-20 23:25:48 +0200

    add missing import

--- packages/supabase_flutter/test/storage_test.dart
+++ packages/supabase_flutter/test/storage_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
