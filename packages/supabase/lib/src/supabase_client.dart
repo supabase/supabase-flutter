c082e554b89d05a53b1164217a598dbe2d725c4c
Vinzent <hello@vinzentw.com>
2026-07-09 00:48:44 +0200

    style: format

--- packages/supabase/lib/src/supabase_client.dart
+++ packages/supabase/lib/src/supabase_client.dart
@@ -363,7 +363,8 @@ class SupabaseClient {
       logLevel: options.logLevel,
       httpClient: _authHttpClient,
       timeout: options.timeout ?? RealtimeConstants.defaultTimeout,
-      connectionCloseTimeout: options.connectionCloseTimeout ??
+      connectionCloseTimeout:
+          options.connectionCloseTimeout ??
           RealtimeConstants.defaultConnectionCloseTimeout,
       customAccessToken: accessToken,
       transport: options.transport,
