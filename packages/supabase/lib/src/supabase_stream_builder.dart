cace5bcf3c2b9e0d2a63662980fcf98debfe132c
Vinzent <hello@vinzentw.com>
2026-07-17 00:09:22 +0200

    feat: support multiple filters on stream method

--- packages/supabase/lib/src/supabase_stream_builder.dart
+++ packages/supabase/lib/src/supabase_stream_builder.dart
@@ -58,9 +58,8 @@ class SupabaseStreamBuilder extends Stream<SupabaseStreamEvent> {
   /// Contains the combined data of postgrest and realtime to emit as stream.
   SupabaseStreamEvent _streamData = [];
 
-  /// `eq` filter used for both postgrest and realtime
-  // ignore: avoid-unassigned-fields
-  _StreamPostgrestFilter? _streamFilter;
+  /// Filters to be applied to the stream
+  List<_StreamPostgrestFilter> _streamFilter = [];
 
   /// Which column to order by and whether it's ascending
   _Order? _orderBy;
@@ -147,16 +146,16 @@ class SupabaseStreamBuilder extends Stream<SupabaseStreamEvent> {
   }
 
   void _getStreamData() {
-    final currentStreamFilter = _streamFilter;
     _streamData = [];
-    PostgresChangeFilter? realtimeFilter;
-    if (currentStreamFilter != null) {
-      realtimeFilter = PostgresChangeFilter(
-        type: currentStreamFilter.type,
-        column: currentStreamFilter.column,
-        value: currentStreamFilter.value,
-      );
-    }
+    List<PostgresChangeFilter> realtimeFilter = _streamFilter
+        .map(
+          (filter) => PostgresChangeFilter(
+            column: filter.column,
+            type: filter.type,
+            value: filter.value,
+          ),
+        )
+        .toList();
 
     _channel = _realtimeClient.channel(
       _realtimeTopic,
@@ -170,7 +169,7 @@ class SupabaseStreamBuilder extends Stream<SupabaseStreamEvent> {
           event: PostgresChangeEvent.all,
           schema: _schema,
           table: _table,
-          filter: realtimeFilter,
+          filters: realtimeFilter,
           callback: (payload) {
             switch (payload.eventType) {
               case PostgresChangeEvent.insert:
@@ -226,59 +225,59 @@ class SupabaseStreamBuilder extends Stream<SupabaseStreamEvent> {
 
   Future<void> _getPostgrestData() async {
     PostgrestFilterBuilder<PostgrestList> query = _queryBuilder.select();
-    if (_streamFilter != null) {
-      query = switch (_streamFilter!.type) {
+    for (final filter in _streamFilter) {
+      query = switch (filter.type) {
         PostgresChangeFilterType.eq => query.eq(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.neq => query.neq(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.lt => query.lt(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.lte => query.lte(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.gt => query.gt(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.gte => query.gte(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.inFilter => query.inFilter(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.like => query.like(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.ilike => query.ilike(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.match => query.matchRegex(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.imatch => query.imatchRegex(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.isFilter => query.isFilter(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
         PostgresChangeFilterType.isDistinct => query.isDistinct(
-          _streamFilter!.column,
-          _streamFilter!.value,
+          filter.column,
+          filter.value,
         ),
       };
     }
--- packages/supabase/lib/src/supabase_stream_filter_builder.dart
+++ packages/supabase/lib/src/supabase_stream_filter_builder.dart
@@ -13,210 +13,184 @@ class SupabaseStreamFilterBuilder extends SupabaseStreamBuilder {
 
   /// Filters the results where [column] equals [value].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).eq('name', 'Supabase');
   /// ```
-  SupabaseStreamBuilder eq(String column, Object value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder eq(String column, Object value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.eq,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] does not equal [value].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).neq('name', 'Supabase');
   /// ```
-  SupabaseStreamBuilder neq(String column, Object value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder neq(String column, Object value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.neq,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is less than [value].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).lt('likes', 100);
   /// ```
-  SupabaseStreamBuilder lt(String column, Object value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder lt(String column, Object value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.lt,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is less than or equal to [value].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).lte('likes', 100);
   /// ```
-  SupabaseStreamBuilder lte(String column, Object value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder lte(String column, Object value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.lte,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is greater than [value].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).gt('likes', '100');
   /// ```
-  SupabaseStreamBuilder gt(String column, Object value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder gt(String column, Object value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.gt,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is greater than or equal to [value].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).gte('likes', 100);
   /// ```
-  SupabaseStreamBuilder gte(String column, Object value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder gte(String column, Object value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.gte,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is included in [values].
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).inFilter('name', ['Andy', 'Amy', 'Terry']);
   /// ```
-  SupabaseStreamBuilder inFilter(String column, List<Object> values) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder inFilter(String column, List<Object> values) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.inFilter,
       column: column,
       value: values,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] matches the [pattern] case-sensitive.
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).like('title', '%foo%');
   /// ```
-  SupabaseStreamBuilder like(String column, String pattern) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder like(String column, String pattern) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.like,
       column: column,
       value: pattern,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] matches the [pattern] case-insensitive.
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).ilike('title', '%foo%');
   /// ```
-  SupabaseStreamBuilder ilike(String column, String pattern) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder ilike(String column, String pattern) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.ilike,
       column: column,
       value: pattern,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] matches the POSIX [regex] case-sensitive.
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).match('slug', r'^post-\d+$');
   /// ```
-  SupabaseStreamBuilder match(String column, String regex) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder match(String column, String regex) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.match,
       column: column,
       value: regex,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] matches the POSIX [regex] case-insensitive.
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).imatch('slug', r'^post-\d+$');
   /// ```
-  SupabaseStreamBuilder imatch(String column, String regex) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder imatch(String column, String regex) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.imatch,
       column: column,
       value: regex,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is `null`, `true` or `false`.
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).isFilter('data', null);
   /// ```
-  SupabaseStreamBuilder isFilter(String column, bool? value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder isFilter(String column, bool? value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.isFilter,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 
   /// Filters the results where [column] is not equal to [value] treating `null
   /// as a distinct value.
   ///
-  /// Only one filter can be applied to `.stream()`.
-  ///
   /// ```dart
   /// supabase.from('users').stream(primaryKey: ['id']).isDistinct('age', null);
   /// ```
-  SupabaseStreamBuilder isDistinct(String column, Object? value) {
-    _streamFilter = (
+  SupabaseStreamFilterBuilder isDistinct(String column, Object? value) {
+    _streamFilter.add((
       type: PostgresChangeFilterType.isDistinct,
       column: column,
       value: value,
-    );
+    ));
     return this;
   }
 }
