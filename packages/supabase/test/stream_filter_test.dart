f18c8552ba7069128c74b6ee9fd97b25f2febc27
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-08-05 16:50:24 +0200

    test: provide asyncStorage in tests added since the PR was opened
    The constructor assertion trips every GoTrueClient and SupabaseClient built
    without a pkce storage, including the suites main gained while this branch sat
    on the old v3 base.

--- packages/supabase/test/stream_filter_test.dart
+++ packages/supabase/test/stream_filter_test.dart
@@ -8,6 +8,8 @@ import 'package:web_socket_channel/io.dart';
 
 import 'package:supabase_common/testing.dart';
 
+import 'utils.dart';
+
 /// Asserts how the filters of `stream()` are sent to the realtime server and to
 /// PostgREST, without needing either of them to be running.
 void main() {
@@ -36,6 +38,7 @@ void main() {
     supabase = SupabaseClient(
       'http://${InternetAddress.loopbackIPv4.address}:${mockServer.port}',
       localStackServiceRoleKey,
+      authOptions: AuthClientOptions(pkceAsyncStorage: TestAsyncStorage()),
     );
   });
 
