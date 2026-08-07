f81924d57b87e2c51ca4e252d581ce98e1ea35ab
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-08-07 16:45:50 +0200

    refactor(storage): move toQueryParams onto TransformOptions
    The ToQueryParams extension added a getter to a class in the same
    library, so it can be a plain member instead. Marked @internal to keep
    it out of the public API, matching DownloadBehavior.queryValue.

--- packages/storage_client/lib/src/types.dart
+++ packages/storage_client/lib/src/types.dart
@@ -675,9 +675,9 @@ class TransformOptions {
     this.quality,
     this.format,
   });
-}
 
-extension ToQueryParams on TransformOptions {
+  /// The transformation options as `transform` query parameters.
+  @internal
   Map<String, String> get toQueryParams {
     return {
       if (width != null) 'width': '$width',
